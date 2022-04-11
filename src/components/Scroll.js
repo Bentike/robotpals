441583aa4ad9e67d4d52f147c4e22154655d7243
ajadi yusuf <yusufajadi93@gmail.com>
2022-04-11 12:48:41 +0100

    changed scroll border to light green

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Scroll extends React.Component{
     render(){
         return(
-            <div style={{overflowY: 'scroll', border: '5px solid lightGray', height: '77vh'}}>
+            <div style={{overflowY: 'scroll', border: '5px solid lightGreen', height: '77vh'}}>
                 {this.props.children}
             </div>
         )
